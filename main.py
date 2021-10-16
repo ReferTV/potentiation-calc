5b951db561dfe4d84f06838c622cd56bd8c3d73f
ReferTV <HeCorepomoc@gmail.com>
2021-10-16 21:15:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Wprowadź ilość boków")
+a = input()
+a = int(a)
+wynik = (a-2)*a/2
+suma_kątów = (a-3)*180
+print ("Ilość przekątnych wynosi", wynik)
+print ("Suma wszystkich kątów w tym wielokącie wynosi", suma_kątów)
